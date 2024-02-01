07a64292d104bb6598d7e1b6ae7496b16f0b4af1
FBe9 <nereaoceja2003@gmail.com>
2024-02-01 23:23:22 +0100

    Test no funciona fecha

--- test/view/unit/UnitWindowControllerTest.java
+++ test/view/unit/UnitWindowControllerTest.java
@@ -1,8 +1,12 @@
 package view.unit;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -12,16 +16,19 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import main.UnitMain;
-import models.LanguageType;
-import models.LevelType;
 import models.Subject;
 import models.Unit;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
 
 /**
  *
@@ -80,21 +87,28 @@ public class UnitWindowControllerTest extends ApplicationTest {
     /**
      * Test of handelCreateButtonAction method, of class UnitWindowController.
      */
-    //@Ignore
+    @Ignore
     @Test
     public void A_testHandelCreateButtonAction() {
         tbvUnit = lookup("#tbvUnit").query();
+        cbSubjects = lookup("#cbSubjects").query();
+        clickOn(cbSubjects);
+        push(KeyCode.DOWN);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
         clickOn("#btnCreateUnit");
+        verifyThat("Unit added successfully in ADT subject", isVisible());
+        press(KeyCode.ENTER);
         Integer count = tbvUnit.getItems().size();
         ObservableList<Unit> userUnits = tbvUnit.getItems();
 
         //Hay una fila de más en la tabla despues de usar el metodo.
-        //assertEquals(tbvUnit.getItems().size(), count + 1);
+        //assertEquals(count + 1, tbvUnit.getItems().size());
         Integer unitRow = 0;
         Unit newUnit = new Unit();
         for (int i = 0; i < userUnits.size(); i++) {
             if (userUnits.get(i).getName().equalsIgnoreCase("")) {
-                if (userUnits.get(i).getSubject().getName().equalsIgnoreCase("PGR")) {
+                if (userUnits.get(i).getSubject().getName().equalsIgnoreCase((String) cbSubjects.getSelectionModel().getSelectedItem())) {
                     newUnit = userUnits.get(i);
                     unitRow = tbvUnit.getItems().indexOf(newUnit);
                     i = userUnits.size();
@@ -103,18 +117,17 @@ public class UnitWindowControllerTest extends ApplicationTest {
         }
 
         //Pasar las fechas a LocalDate para compararlas
-        Date dateInit = newUnit.getDateInit();
+        /*Date dateInit = newUnit.getDateInit();
         LocalDate dateInitLocal = dateInit.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         Date dateEnd = newUnit.getDateEnd();
-        LocalDate dateEndLocal = dateEnd.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
+        LocalDate dateEndLocal = dateEnd.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();*/
         //Verificar que la nueva fila tiene los mismos datos que la creada por defecto
-        assertEquals(newUnit.getName(), "");
-        assertEquals(newUnit.getSubject(), "PGR");
-        assertEquals(newUnit.getDescription(), "");
-        assertEquals(dateInitLocal, LocalDate.now());
-        assertEquals(dateEndLocal, LocalDate.now());
-        assertEquals(newUnit.getHours(), "0");
+        assertEquals(null, newUnit.getName());
+        assertEquals("ADT", newUnit.getSubject().getName());
+        assertEquals(null, newUnit.getDescription());
+        // assertEquals(LocalDate.now(), dateInitLocal);
+        // assertEquals(LocalDate.now(), dateEndLocal);
+        assertEquals("0", newUnit.getHours());
 
         //Borrar prueba en la trabla
         Node row = lookup(".table-row-cell").nth(unitRow).query();
@@ -123,11 +136,110 @@ public class UnitWindowControllerTest extends ApplicationTest {
     }
 
     /**
-     * Test of handelSearchButtonAction method, of class UnitWindowController.
+     * Test of search by name method, of class UnitWindowController.
      */
     @Ignore
     @Test
-    public void testHandelSearchButtonAction() {
+    public void B_testHandelSearchName() {
+        cbSubjects = lookup("#cbSubjects").query();
+        cbSearchType = lookup("#cbSearchType").query();
+        tfSearch = lookup("#tfSearch").query();
+        btnSearch = lookup("#btnSearch").query();
+        tbvUnit = lookup("#tbvUnit").query();
+        clickOn(cbSubjects);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(tfSearch);
+        write("UD");
+        clickOn(btnSearch);
+        ObservableList<Unit> userUnits = tbvUnit.getItems();
+        for (Unit unit : userUnits) {
+            assertTrue(unit.getName().contains("UD"));
+        }
+    }
+
+    /**
+     * Test of search by date init method, of class UnitWindowController.
+     */
+    @Ignore
+    @Test
+    public void C_testHandelSearchDateInit() {
+        cbSubjects = lookup("#cbSubjects").query();
+        cbSearchType = lookup("#cbSearchType").query();
+        dpSearch = lookup("#dpSearch").query();
+        btnSearch = lookup("#btnSearch").query();
+        tbvUnit = lookup("#tbvUnit").query();
+        clickOn(cbSubjects);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(cbSearchType);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(dpSearch);
+        write("31-ene-2024");
+        clickOn(btnSearch);
+        ObservableList<Unit> userUnits = tbvUnit.getItems();
+        for (Unit unit : userUnits) {
+            //Lo pasa a localdate para poder compararlo.
+
+        }
+    }
+
+    /**
+     * Test of search by date end method, of class UnitWindowController.
+     */
+    @Ignore
+    @Test
+    public void D_testHandelSearchDateEnd() {
+        cbSubjects = lookup("#cbSubjects").query();
+        cbSearchType = lookup("#cbSearchType").query();
+        dpSearch = lookup("#dpSearch").query();
+        btnSearch = lookup("#btnSearch").query();
+        tbvUnit = lookup("#tbvUnit").query();
+        clickOn(cbSubjects);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(cbSearchType);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(dpSearch);
+        write("31-ene-2024");
+        clickOn(btnSearch);
+        ObservableList<Unit> userUnits = tbvUnit.getItems();
+        for (Unit unit : userUnits) {
+            //Lo pasa a localdate para poder compararlo.
+
+        }
+    }
+
+    /**
+     * Test of search by hours method, of class UnitWindowController.
+     */
+    @Ignore
+    @Test
+    public void E_testHandelSearchHours() {
+        cbSubjects = lookup("#cbSubjects").query();
+        cbSearchType = lookup("#cbSearchType").query();
+        tfSearch = lookup("#tfSearch").query();
+        btnSearch = lookup("#btnSearch").query();
+        tbvUnit = lookup("#tbvUnit").query();
+        clickOn(cbSubjects);
+        push(KeyCode.DOWN);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(cbSearchType);
+        push(KeyCode.DOWN);
+        push(KeyCode.DOWN);
+        push(KeyCode.DOWN);
+        press(KeyCode.ENTER);
+        clickOn(tfSearch);
+        write("2");
+        clickOn(btnSearch);
+        ObservableList<Unit> userUnits = tbvUnit.getItems();
+        for (Unit unit : userUnits) {
+            //Lo pasa a localdate para poder compararlo.
+            assertEquals(unit.getHours(), "2");
+        }
     }
 
     /**
@@ -135,7 +247,77 @@ public class UnitWindowControllerTest extends ApplicationTest {
      */
     @Ignore
     @Test
-    public void testHandelDeleteButtonAction() {
+    public void F_testHandelDeleteButtonAction() {
+        tbvUnit = lookup("#tbvUnit").query();
+        Integer roxNum = tbvUnit.getItems().size();
+        Node row = lookup(".table-row-cell").nth(0).query();
+        clickOn(row);
+        Unit deletedUnit = tbvUnit.getSelectionModel().getSelectedItem();
+        clickOn("#btnDeleteUnit");
+        verifyThat("¿Are you sure you want to delete this unit?", isVisible());
+        press(KeyCode.ENTER);
+        Node row2 = lookup(".table-row-cell").nth(0).query();
+        clickOn(row2);
+        Unit compareUnit = tbvUnit.getSelectionModel().getSelectedItem();
+        assertNotEquals(compareUnit.getId(), deletedUnit.getId());
+        ObservableList<Unit> userUnits = tbvUnit.getItems();
+        Boolean notFound = true;
+        for (Unit unit : userUnits) {
+            if (unit.getName().equalsIgnoreCase(deletedUnit.getName())) {
+                if (unit.getSubject().equals(deletedUnit.getSubject())) {
+                    notFound = false;
+                }
+            }
+        }
+        assertTrue(notFound);
+    }
+
+    /**
+     * Test of update name method, of class UnitWindowController.
+     */
+    //@Ignore
+    @Test
+    public void G_testHandelUpdateName() {
+        tbvUnit = lookup("#tbvUnit").query();
+        Node row = lookup(".table-row-cell").nth(0).query();
+        clickOn(row);
+        //Seleccionar el nodo de todas las filas para poder hacer click en ella.
+        Integer tablerow = tbvUnit.getSelectionModel().getSelectedIndex();
+        Node tbcNameNode = lookup("#tbcName").nth(tablerow + 1).query();
+        Node tbcSubjectNode = lookup("#tbcSubject").nth(tablerow + 1).query();
+        Node tbcDescriptionNode = lookup("#tbcDescription").nth(tablerow + 1).query();
+        Node tbcDateInitNode = lookup("#tbcDateInit").nth(tablerow + 1).query();
+        Node tbcDateEndNode = lookup("#tbcDateEnd").nth(tablerow + 1).query();
+        Node tbcHoursNode = lookup("#tbcHours").nth(tablerow + 1).query();
+
+        Unit unitSelected = (Unit) tbvUnit.getSelectionModel().getSelectedItem();
+        String name = unitSelected.getName();
+        Subject subject = unitSelected.getSubject();
+        String description = unitSelected.getDescription();
+        String hours = unitSelected.getHours();
+
+        //Click en las diferentes celdas para ir modificando los datos
+        clickOn(tbcNameNode);
+        write("prueba");
+        push(KeyCode.ENTER);
+        doubleClickOn(tbcSubjectNode);
+        push(KeyCode.DOWN);
+        clickOn(tbcDescriptionNode);
+        write("prueba desc");
+        push(KeyCode.ENTER);
+        clickOn(tbcHoursNode);
+        write("999");
+        push(KeyCode.ENTER);
+        doubleClickOn(tbcSubjectNode);
+        push(KeyCode.DOWN);
+        Unit modifiedUnit = (Unit) tbvUnit.getSelectionModel().getSelectedItem();
+
+        assertNotEquals(name, modifiedUnit.getName());
+        assertNotEquals(subject.getName(), modifiedUnit.getSubject().getName());
+        assertNotEquals(description, modifiedUnit.getDescription());
+        //date
+        assertNotEquals(hours, modifiedUnit.getHours());
+
     }
 
 }
